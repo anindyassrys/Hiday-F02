d53f0a8c37b31c5beac3daa9d88165ea7cfdd46f
anindyasasriya <anindya.sasriya@ui.ac.id>
2022-05-15 16:34:48 +0700

    Update and Read FE paket_koin

--- paket_koin/urls.py
+++ paket_koin/urls.py
@@ -5,5 +5,6 @@ app_name = 'paket_koin'
 
 urlpatterns = [
     path('', views.list_paket_koin, name='list_paket_koin'),
-    path('create_paket_koin', views.create_paket_koin, name='create_paket_koin')
+    path('create-paket-koin', views.create_paket_koin, name='create_paket_koin'),
+    path('update-paket-koin/<int:value>/<int:harga>', views.update_paket_koin, name='update_paket_koin')
 ]
--- paket_koin/views.py
+++ paket_koin/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.db import connection
 
-# Create your views here.
+# Create paket
 def create_paket_koin(request):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO public")
@@ -9,11 +9,12 @@ def create_paket_koin(request):
         role = request.session['email'][1]
         if role == "admin":
             if request.method == "POST":
+                print("bryan gak ganteng")
                 jumlah_koin = request.POST["jumlah_koin"]
                 harga = request.POST["harga"]
                 return redirect("paket_koin:list_paket")
             else:
-                return render(request, ".html", {})
+                return render(request, "create_paket_koin.html", {})
         else:
             return redirect("paket_koin:list_paket_koin")
     else:
@@ -21,8 +22,10 @@ def create_paket_koin(request):
 
 # Read paket_koin
 def list_paket_koin(request):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
     if request.session.has_key('email'):
-        cursor = connection.cursor()
+        cursor.execute("SET search_path TO hidayf02")
 
         if (request.session['email'][1] == "admin"):
             cursor.execute("SELECT * FROM paket_koin")
@@ -34,5 +37,22 @@ def list_paket_koin(request):
             role = "pengguna"
         
     return render(request, 'list_paket_koin.html', {'results': result, 'role': role})
+
+#update paket_koin
+def update_paket_koin(request, value, harga):
+    cursor = connection.cursor()
+    cursor.execute("SET search_path TO public")
+    
+    if request.session.has_key('email'):
+        role = request.session['email'][1]
+        if role == "admin":
+            if request.method == "POST":
+                return redirect("paket_koin:list_paket")
+            else:
+                return render(request, 'ubah_paket_koin.html', {'value': value, 'role': role, 'harga':harga})
+        else:
+            return redirect("paket_koin:list_paket_koin")
+    else:
+        return redirect("home:login")
 
 
