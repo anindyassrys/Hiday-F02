d579c8d61c46f8a5e496f75a4d777bed581f281b
Mohammad Rizky Chairul Azizi <rizky.ca16@gmail.com>
2022-05-16 20:52:16 +0700

    Revert "Merge branch 'kesya' into 'main'"
    This reverts commit b8b804ad523e37be4499ad03d11e4701145e0146, reversing
    changes made to d9ffb013caf0572aeeda9ddead64e31f0978afbb.

--- koleksi_aset/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class KoleksiAsetConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'koleksi_aset'
--- transaksi_pembelian_aset/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TransaksiPembelianAsetConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transaksi_pembelian_aset'
