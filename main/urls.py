fb4270abdc6712325e497861435bd37c4d411cf8
Fairuza Raryasdya <fairuza.raryasdya@gmail.com>
2022-05-22 14:42:14 +0000

    Revert "Merge branch 'atha's-branch' into 'main'"
    This reverts merge request !16

--- main/urls.py
+++ main/urls.py
@@ -12,11 +12,5 @@ urlpatterns = [
     path('produksi', views.produksi, name = 'produksi'),
     path('histori_produk_makanan', views.histori_produk_makanan, name = 'histori_produk_makanan'),
     path('lihat_isi_lumbung', views.lihat_isi_lumbung, name = 'lihat_isi_lumbung'),
-    path('produksi/<slug:slug>', views.produksi_details, name = 'produk_details'),
-    path('create_produk', views.create_produk, name = 'create_produk'),
-    path('update_produk', views.update_produk, name = 'update_produk'),
-    path('create_produksi', views.create_produksi, name = 'create_produksi'),
-    path('update_produksi', views.update_produksi, name = 'update_produksi'),
-    path('create_histori_produk_makanan', views.create_histori_produk_makanan, name = 'create_histori_produk_makanan')
-    
+    path('produksi/<slug:slug>', views.produksi_details, name = 'produk_details')
 ]
